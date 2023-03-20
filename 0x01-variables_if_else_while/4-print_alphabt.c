053d4b1cec3f7343ab984fd32dd2d8cf1c42b3f1
curious coddes <curiouscoddes@gmail.com>
2023-03-20 10:36:01 -0700

    Print aphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints all the aphabet except q and e
+ *
+ * Return: returns 0 (successful)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(alp[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
