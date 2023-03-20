00eb7d92845ec527fb3035a800d724ba1f0cd461
curious coddes <curiouscoddes@gmail.com>
2023-03-20 12:40:47 -0700

    Prints alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in reverse
+ *
+ * Return: returns 0 (success)
+ *
+ *
+ */
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
