6cd561ca85fd2eed3443ea04fcbaecd7febcadaa
curious coddes <curiouscoddes@gmail.com>
2023-03-20 09:33:39 -0700

    Prints Alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet characters in lowercase
+ *
+ * Return: Always 0 a (success)
+ *
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqsrtuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar("\n");
+	return (0);
+}
