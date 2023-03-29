1af6854e7576321071a568f1c6d7cffd6a9e1192
curious coddes <curiouscoddes@gmail.com>
2023-03-29 03:28:07 +0000

    created the _putchar() function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 /**
- * main - prints _putchar on the output
- *
- * Return: returns (0) (success)
- *
- */
+ *  * main - prints the name _putchar on the stdout
+ *   * _putchar - writes the character c to stdout
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *       */
 int main(void)
 {
-	putchar('\n');
+       	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
