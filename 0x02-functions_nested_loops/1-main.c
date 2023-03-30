d9e31513b908ac0ac5d4911ab8c97d87b8fa2168
curious coddes <curiouscoddes@gmail.com>
2023-03-30 19:20:40 +0000

    created void print_alphabet(void), void print_alphabet_x10(void) and int _islower(int c) finctions

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - verify that print_alphabet() function prints alphabets in lower case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
