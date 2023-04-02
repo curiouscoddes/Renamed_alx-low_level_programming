9ba645df88111146adb8cc5aff58674bf90d4c50
curious coddes <curiouscoddes@gmail.com>
2023-04-02 19:06:25 +0000

    Created add(), times_table() and 24_hours()

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds 2 numbers
+ * @a: funtion param
+ * @b: function param
+ * Return: returns the result (add)
+ *
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
